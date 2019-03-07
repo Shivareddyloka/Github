9f2879550b937e42c37e4bcaa9d2d76c9a27ca3f
Shivareddyloka <shivareddyloka@gmail.com>
2019-03-07 11:09:40 +0530

    HI!This is shiva!

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAdC5ZjvVhL5Eyb9413SVHK4Mke1IscgM4',
+    authDomain: 'fir-weatherapp-df7cb.firebaseapp.com',
+    databaseURL: 'https://fir-weatherapp-df7cb.firebaseio.com',
+    projectId: 'fir-weatherapp-df7cb',
+    storageBucket: 'fir-weatherapp-df7cb.appspot.com',
+    messagingSenderId: '128713799213'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDRPVU9vhOpt3tq4asZMK4sJgzLXZZYnM8",
+    authDomain: "weather-b933f.firebaseapp.com",
+    databaseURL: "https://weather-b933f.firebaseio.com",
+    projectId: "weather-b933f",
+    storageBucket: "weather-b933f.appspot.com",
+    messagingSenderId: "745343138080"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
